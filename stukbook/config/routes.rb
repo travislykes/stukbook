cea744b60c3f6748afb7bc1522516db7f62fe093
Francis Opoku-Amoako <fopokuamoako@Franciss-MacBook-Pro.local>
2016-10-05 14:52:28 +0000

    Added User Profile with gravatat an tinns

--- stukbook/config/routes.rb
+++ stukbook/config/routes.rb
@@ -1,10 +1,14 @@
 Stukbook::Application.routes.draw do
  
+  #get 'users/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   devise_for :users, controllers: {registrations: 'registrations'}
   # You can have the root of your site routed with "root"
   root 'pages#home'
+
+  resources :users, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
