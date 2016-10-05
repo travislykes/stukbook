ee80b9f2090cb553e58c1a5f10e642663ef2da2b
Francis Opoku-Amoako <fopokuamoako@Franciss-MacBook-Pro.local>
2016-10-05 17:14:59 +0000

    Added database seed file

--- stukbook/db/seeds.rb
+++ stukbook/db/seeds.rb
@@ -1,7 +1,15 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "tlykes87", email: "lykesm87@gmail.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "lykesm88", email: "lykesm88@gmail.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "lykesm89", email: "lykesm89@gmail.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "lykesm90", email: "lykesm90@gmail.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "lykesm91", email: "lykesm91@gmail.com", password: "lovestuk", password_confirmation: "lovestuk")
+p "Test users created"
