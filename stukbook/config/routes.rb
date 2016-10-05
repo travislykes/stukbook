88fe85f5494f120bf2599c52cb4c8b165e2e7955
Francis Opoku-Amoako <fopokuamoako@Franciss-MacBook-Pro.local>
2016-10-05 14:33:07 +0000

    Added Devise and created registrations controller

--- stukbook/config/routes.rb
+++ stukbook/config/routes.rb
@@ -2,7 +2,7 @@ Stukbook::Application.routes.draw do
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  devise_for :users, controllers: {registrations: 'registrations'}
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
