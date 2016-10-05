78b7c0f5a38b69002247f995157fa8160d0f5b2a
Francis Opoku-Amoako <fopokuamoako@Franciss-MacBook-Pro.local>
2016-10-05 15:45:06 +0000

    Added extra user attributes

--- stukbook/app/controllers/users/registrations_controller.rb
+++ stukbook/app/controllers/users/registrations_controller.rb
@@ -62,6 +62,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
+      params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password, :age, :bio, :gender)
   end
 end
