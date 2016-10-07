1fcfb21812a16627de1cb62918f7ccc2ed64d2e4
Francis Opoku-Amoako <fopokuamoako@Franciss-MacBook-Pro.local>
2016-10-07 16:51:44 +0000

    Added post model

--- stukbook/db/schema.rb
+++ stukbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007151250) do
+ActiveRecord::Schema.define(version: 20161007164752) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "trackable_type"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20161007151250) do
     t.datetime "friended_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.text     "content_html"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
